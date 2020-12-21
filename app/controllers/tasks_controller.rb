cb3ed3514b7264183a2135afb149794a1e8fdcf8
tsumutsumu <noritsumu0208@gmail.com>
2020-12-21 09:09:10 +0000

    four times

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
    
   def index
     @tasks = Task.all
@@ -58,9 +57,6 @@ class TasksController < ApplicationController
     end
   end
   
-  def set_task
-    @task = Task.find(params[:id])  
-  end
   
 end
 
